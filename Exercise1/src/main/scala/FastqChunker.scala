7fce53b9aa86bdfebbb2e17ee47afebc612cf22b
Gabriel Martins Souto <gsouto@kova-01.ewi.tudelft.nl>
2016-11-01 03:45:03 +0100

    working

--- Exercise1/src/main/scala/FastqChunker.scala
+++ Exercise1/src/main/scala/FastqChunker.scala
@@ -4,12 +4,8 @@ import org.apache.spark.SparkConf
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-
-import org.apache.hadoop.conf.Configuration
-//import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
-import org.apache.hadoop.io.Text
-import org.apache.hadoop.io.LongWritable
+import org.apache.hadoop.io.compress.GzipCodec
+import java.util.zip._
 
 import java.io._
 import java.nio.file.{Paths, Files}
@@ -42,8 +38,6 @@ object FastqChunker
 		val conf = new SparkConf().setAppName("DNASeqAnalyzer")
 		conf.setMaster("local[" + prllTasks + "]")
 		conf.set("spark.cores.max", prllTasks)	
-		conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-		conf.set("spark.kryo.classesToRegister", "org.apache.hadoop.io.LongWritable,org.apache.hadoop.io.Text")
 		
 
 		val sc = new SparkContext(conf)
@@ -51,26 +45,59 @@ object FastqChunker
 		// Comment these two lines if you want to see more verbose messages from Spark
 		Logger.getLogger("org").setLevel(Level.OFF);
 		Logger.getLogger("akka").setLevel(Level.OFF);	
-
-		val readConf = new Configuration
-		//readConf.set("textinputformat.record.delimiter", "\n\n")
-		readConf.set("textinputformat.linespermap", "4")
 			
 		var t0 = System.currentTimeMillis
 		
 		// Rest of the code goes here 
 
 
-		val first_avroRdd = sc.newAPIHadoopFile(args(1) + "/fastq1.fq", classOf[TextInputFormat], classOf[LongWritable], classOf[Text], readConf).setName("rdd_first_read")
+		//Read the line
+		//Associate every line with an id
+		//Since every 4 lines should be associated with the same id, it's divided by 4 
+		//And we map the number to the key
+		//Then we reduce so that the 4 lines join each other becomes together
+		//(Id, DNA sequence)
+		//(Long, Text)
+		val first_avroRdd =  sc.textFile(args(1) + "/fastq1.fq").zipWithIndex().mapPartitions(
+			lines => 
+			{
+			for(line <- lines)
+		 		yield (line._2 /4 , line._1)  
+		 	}).reduceByKey(_ + "\n"+ _, prllTasks.toInt).setName("rdd_first_read")
+		
+		//Read the line
+		//Associate every line with an id
+		//Since every 4 lines should be associated with the same id, it's divided by 4 
+		//And we map the number to the key
+		//(Id, DNA sequence)
+		//(Long, Text)
+		val second_avroRdd = sc.textFile(args(1) + "/fastq2.fq").zipWithIndex().mapPartitions(lines => 
+			{
+			for(line <- lines)
+		 		yield (line._2 /4 , line._1)  
+		 	} ).reduceByKey(_+ "\n" +_, prllTasks.toInt).setName("rdd_second_read")
 
-		first_avroRdd.take(10).foreach(rdd => println(rdd._2.toString.split("\n").length +": " + rdd._2))
+		//(Id, ())
+		val joined = first_avroRdd.join(second_avroRdd).sortByKey(true).setName("rdd_joined")
+		
 
 		
-		//fastq2.fq
 
-
-
+		val outputRDD = joined.mapPartitionsWithIndex((index: Int, it: Iterator[(Long, (String, String))]) => {
+			val bw = new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(outputFolder+ "/" + index + ".fq.gz")), "UTF-8"))
+			
+			for(line <- it)
+			{
+				bw.write(line._2._1 + "\n" + line._2._2)
+				bw.newLine()
+			}
 		
+			bw.close()
+
+			it
+			} ).setName("rdd_output")
+
+		outputRDD.count
 		val et = (System.currentTimeMillis - t0) / 1000 
 		println("|Execution time: %d mins %d secs|".format(et/60, et%60))
 	}
